5f23cb6536a56fa09eba14259ec0bf980a5e3cbe
amh21-Ha <amha.ha07@gmail.com>
2022-10-21 10:58:22 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -3,10 +3,10 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
-* _printf - Printf function
-* @format: format.
-* Return: Printed chars.
-*/
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 
 int _printf(const char *format, ...)
 {
@@ -18,7 +18,6 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-	
 	va_start(list, format);
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
@@ -54,25 +53,14 @@ int _printf(const char *format, ...)
 
 
     /**
-
      * print_buffer - Prints the contents of the buffer if it exist
+     * @buffer: Array of chars
+     * @buff_ind: Index at which to add next char, represents the length.
+     */
 
-      * @buffer: Array of chars
-
-       * @buff_ind: Index at which to add next char, represents the length.
-
-        */
-
-	void print_buffer(char buffer[], int *buff_ind)
-
-	{
-
-			if (*buff_ind > 0)
-
-						write(1, &buffer[0], *buff_ind);
-
-
-
-							*buff_ind = 0;
-
-
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+	*buff_ind = 0;
+}
